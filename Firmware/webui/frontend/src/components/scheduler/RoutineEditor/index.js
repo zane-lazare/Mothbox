08aa338eb030090725da2f1da654907cc0e372a4
Zane Lazare <zane@lazare.nz>
2026-03-07 19:06:22 +1300

    chore(#494): remove dead RoutineEditor, ActionForm, and OffsetTimeline components

--- Firmware/webui/frontend/src/components/scheduler/RoutineEditor/index.js
+++ Firmware/webui/frontend/src/components/scheduler/RoutineEditor/index.js
@@ -3,16 +3,8 @@
  *
  * Components for creating and editing routines within schedules.
  *
- * Main export:
- * - RoutineEditor: Full routine editing form with validation
- *
- * Sub-components (for advanced usage):
+ * Exports:
  * - ActionList: Drag-and-drop list of actions
- * - ActionForm: Modal form for individual actions
- * - OffsetTimeline: Visual timeline of action offsets
  */
 
-export { default as RoutineEditor } from './RoutineEditor'
 export { default as ActionList } from './ActionList'
-export { default as ActionForm } from './ActionForm'
-export { default as OffsetTimeline } from './OffsetTimeline'
