cd686ab289cdf878997cdc76564810072b57f83b
Zane Lazare <zane@lazare.nz>
2026-03-07 19:22:32 +1300

    chore(#498): delete TriggerSelector directory and remaining RoutineEditor files

--- Firmware/webui/frontend/src/components/scheduler/RoutineEditor/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Routine Editor Components (Issue #226, #321)
- *
- * NOTE: ActionList and InlineActionRow have been migrated to
- * ScheduleEditor/ as TypeScript (Issue #497).
- *
- * Remaining files (constants.js) will be cleaned up in Issue #498.
- */
